0bdd173b189a0b04d45ab607fba34c89e911faf7
JSterling56 <jtsterlin@yahoo.com>
2023-08-28 12:51:06 -0400

    refactored JS, began implementing moment in HTML

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,43 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var currentDat = $("#currentDay");
-currentDay.text (moment().format('dddd, MMM, YYYY'));
+$(document).ready(function(){
+  let NowMoment = moment().format("MMMM Do YYYY");
+  let displayDate = document.getElementById("currentDay");
+  displayDate.innerHTML = NowMoment;
+  let currentHour = moment().format("HH");
 
-var currentHour = moment().hour();
+  $("#clearFieldsBtn").click(function(event){
+    event.preventDefault;
+    $("textarea").val("");
+    localStorage.clear();
+  });
 
-$(".time-block").each(function () {
-  var blockHour = $(this).attr("id").split("-")[1];
-  var textEntry = localStorage.getItem(blockHour);
-  var textarea = $(this).find(".description");
-  textarea.val (textEntry);
-  if (blockHour < currentHour){
-      $(this).find(".description").addClass("past");
-  }else if(blockHour === currentHour) {
-      $(this).find(".desciption").addClass("present");
-  }else{
-      $(this).find(".description").addclass("future");
-  }
+  $(".time-div").each(function(){
+    var timeDiv= $(this).attr("id").split("-")[1];
+    if(currentHour == timeDiv){
+      $(this).addClass("present");
+      $(this).children(".description").addClass("white-text");
+    }else if (currentHour < timeDiv){
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }else if (currentHour > timeDiv){
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+  });
 
+  $(".saveBtn").click(function(event){
+    event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
+    });
+
+    $("#hour-09 .time-block").val(localStorage.getItem("09"));
+    $("#hour-10 .time-block").val(localStorage.getItem("10"));
+    $("#hour-11 .time-block").val(localStorage.getItem("11"));
+    $("#hour-12 .time-block").val(localStorage.getItem("12"));
+    $("#hour-13 .time-block").val(localStorage.getItem("13"));
+    $("#hour-14 .time-block").val(localStorage.getItem("14"));
+    $("#hour-15 .time-block").val(localStorage.getItem("15"));
+    $("#hour-16 .time-block").val(localStorage.getItem("16"));
+    $("#hour-17 .time-block").val(localStorage.getItem("17"));
 });
-
-$(".saveBtn").on("click", function(){
-  var key = $(this).parent().attr("id").split("-")[1];
-  var value = $(this).parent().find(".description").val();
-  localStorage.setItem(key.value);
-})
